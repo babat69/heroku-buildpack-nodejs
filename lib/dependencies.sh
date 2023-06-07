abe34f82eb4cb97ab50f78df83d85bc1af905c24
babat69 <97878577+babat69@users.noreply.github.com>
2023-06-07 10:28:39 +0200

    Update dependencies.sh

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -233,7 +233,7 @@ npm_node_modules() {
       else
         echo "Installing node modules (package.json)"
       fi
-      monitor "npm-install" npm install --production="$production" --unsafe-perm --userconfig "$build_dir/.npmrc" 2>&1
+      monitor "npm-install" npm ci --production="$production" --unsafe-perm --userconfig "$build_dir/.npmrc" 2>&1
     fi
   else
     echo "Skipping (no package.json)"
